bbaa97a07c30ee59e8af18cb3572c269c0730549
Travis Veazey <travisvz@gmail.com>
2021-03-22 13:45:39 -0800

    Adding doc blocks to Voronoi functions

--- src/voronoi.rs
+++ src/voronoi.rs
@@ -3,6 +3,10 @@ use rand::distributions::{Distribution, Uniform};
 use rand_xoshiro::Xoshiro256StarStar;
 use delaunator::{Point, Triangulation, triangulate, next_halfedge};
 
+/// A map represented by Voronoi polygons built from the Delaunay triangulation of random points.
+/// 
+/// The implementation of the Voronoi graph from its Delaunay triangulation is based on the article
+/// and code at https://mapbox.github.io/delaunator/
 pub struct Voronoi {
     pub width: u32,
     pub height: u32,
@@ -12,6 +16,14 @@ pub struct Voronoi {
 }
 
 impl Voronoi {
+    /// Return a new Voronoi map.
+    /// 
+    /// # Arguments
+    /// 
+    /// * `seed` - The RNG seed for generating Voronoi "seeds", i.e. the points around which Voronoi cells are built
+    /// * `num_cells` - The number of Voronoi "seeds" to create
+    /// * `width` - The width of the map
+    /// * `height` - The height of the map
     pub fn new(seed: u64, num_cells: usize, width: u32, height: u32) -> Voronoi {
         let mut rng = Xoshiro256StarStar::seed_from_u64(seed);
         let dist_x = Uniform::from(0..width);
@@ -40,6 +52,13 @@ impl Voronoi {
         }
     }
 
+    /// For the specified triangle return its half-edges.
+    /// 
+    /// We define a triangle to be represented by the half-edges that run counter-clockwise around its perimeter.
+    /// 
+    /// # Arguments
+    /// 
+    /// * `triangle` - The index of the triangle to find half-edges for
     pub fn edges_of_triangle(&self, triangle: usize) -> [usize; 3] {
         [
             3 * triangle,
@@ -48,10 +67,20 @@ impl Voronoi {
         ]
     }
 
-    pub fn triangle_of_edge(&self, e: usize) -> usize {
-        e / 3
+    /// Find the triangle the given half-edge belongs to
+    /// 
+    /// # Arguments
+    /// 
+    /// * `halfedge` - The index of the halfedge we want to find the triangle for
+    pub fn triangle_of_edge(&self, halfedge: usize) -> usize {
+        halfedge / 3
     }
 
+    /// Find the points for the given triangle
+    /// 
+    /// # Arguments
+    /// 
+    /// * `triangle` - The index of the triangle to find points for
     pub fn points_of_triangle(&self, triangle: usize) -> Vec::<usize> {
         self.edges_of_triangle(triangle)
             .iter()
@@ -59,6 +88,12 @@ impl Voronoi {
             .collect()
     }
 
+    /// Find the triangles adjacent to the given triangle
+    /// 
+    /// # Arguments
+    /// 
+    /// * `triangle` - The triangle to find neighbors for
+    #[allow(dead_code)]
     pub fn adjacent_triangles(&self, triangle: usize) -> Vec::<usize> {
         self.edges_of_triangle(triangle)
             .iter()
@@ -73,6 +108,7 @@ impl Voronoi {
             .collect()
     }
 
+    /// Find the circumcenter of the points `a`, `b`, and `c`.
     pub fn circumcenter(&self, a: &Point, b: &Point, c: &Point) -> Point {
         let ad = a.x.powi(2) + a.y.powi(2);
         let bd = b.x.powi(2) + b.y.powi(2);
@@ -86,11 +122,13 @@ impl Voronoi {
         }
     }
 
+    /// Find the circumcenter of the given triangle
     pub fn triangle_center(&self, triangle: usize) -> Point {
         let p = self.points_of_triangle(triangle);
         self.circumcenter(&self.seeds[p[0]], &self.seeds[p[1]], &self.seeds[p[2]])
     }
 
+    /// Find the edges that point in to the specified start point.
     pub fn edges_around_point(&self, start: usize) -> Vec<usize> {
         let mut result = Vec::new();
         let mut incoming = start;
