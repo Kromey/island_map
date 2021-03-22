cf5bc99628f9553c70d7ae6af6f7e73c31429546
Travis Veazey <travisvz@gmail.com>
2021-03-22 11:52:39 -0800

    Refactor Delaunay-based Voronoi into struct

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use image;
 use imageproc::drawing::{draw_hollow_circle, draw_line_segment, draw_polygon};
 use noise::{Fbm, Seedable, NoiseFn};
 use std::time::{Instant, Duration};
-use delaunator::{Point, Triangulation, next_halfedge};
+use delaunator::{Point, next_halfedge};
 
 mod voronoi;
-use voronoi::Voronoi;
+//use voronoi::Voronoi;
 mod voronoi2;
-use voronoi2::new_delaunay;
+use voronoi2::Voronoi;
 
-fn draw_voronoi(vor: &Voronoi, imgx: u32, imgy: u32, i: usize, show_water: bool) {
+/*fn draw_voronoi(vor: &Voronoi, imgx: u32, imgy: u32, i: usize, show_water: bool) {
     let mut img = image::ImageBuffer::new(imgx as u32, imgy as u32);
     let interior = image::Rgb([255u8, 255, 255]);
     let frontier = image::Rgb([0u8, 0, 0]);
@@ -59,17 +59,17 @@ fn draw_voronoi(vor: &Voronoi, imgx: u32, imgy: u32, i: usize, show_water: bool)
     }
 
     img.save(format!("map_{:02}.png", i)).unwrap();
-}
+}*/
 
-fn draw_delauney(points: &Vec::<Point>, delauney: &Triangulation, imgx: u32, imgy: u32) {
+fn draw_delaunay(vor: &Voronoi, imgx: u32, imgy: u32) {
     let mut img = image::ImageBuffer::new(imgx as u32, imgy as u32);
     let fill = image::Rgb([221u8, 221, 213]);
     let color_edge = image::Rgb([0u8, 0, 0]);
-    let delauney_point = image::Rgb([255u8, 0, 0]);
+    let delaunay_point = image::Rgb([255u8, 0, 0]);
     let voronoi_corner = image::Rgb([0u8, 0, 255]);
 
-    /*for triangle in 0..delauney.triangles.len() / 3 {
-        let p: Vec::<imageproc::point::Point::<i32>> = voronoi2::points_of_triangle(delauney, triangle)
+    /*for triangle in 0..delaunay.triangles.len() / 3 {
+        let p: Vec::<imageproc::point::Point::<i32>> = voronoi2::points_of_triangle(delaunay, triangle)
             .iter()
             .map(|&e| imageproc::point::Point::new(points[e].x as i32, points[e].y as i32))
             .collect();
@@ -77,26 +77,26 @@ fn draw_delauney(points: &Vec::<Point>, delauney: &Triangulation, imgx: u32, img
         img = draw_polygon(&img, &p, fill);
     }*/
 
-    /*for e in 0..delauney.triangles.len() {
-        if e > delauney.halfedges[e] {
-            let p = &points[delauney.triangles[e]];
-            let q = &points[delauney.triangles[next_halfedge(e)]];
+    /*for e in 0..delaunay.triangles.len() {
+        if e > delaunay.halfedges[e] {
+            let p = &points[delaunay.triangles[e]];
+            let q = &points[delaunay.triangles[next_halfedge(e)]];
 
             img = draw_line_segment(&img, (p.x as f32, p.y as f32), (q.x as f32, q.y as f32), color_edge);
         }
     }*/
 
     println!("\t\tDrawing Voronoi polygons...");
-    let mut seen = vec![false; delauney.triangles.len()];
-    for e in 0..delauney.triangles.len() {
-        let p = delauney.triangles[next_halfedge(e)];
+    let mut seen = vec![false; vor.delaunay.triangles.len()];
+    for e in 0..vor.delaunay.triangles.len() {
+        let p = vor.delaunay.triangles[next_halfedge(e)];
 
         if !seen[p] {
             seen[p] = true;
-            let edges = voronoi2::edges_around_point(delauney, e);
-            let triangles: Vec::<usize> = edges.iter().map(|&e| voronoi2::triangle_of_edge(e)).collect();
+            let edges = vor.edges_around_point(e);
+            let triangles: Vec::<usize> = edges.iter().map(|&e| vor.triangle_of_edge(e)).collect();
             let mut vertices: Vec::<imageproc::point::Point::<i32>> = triangles.iter().map(|&t| {
-                let p = voronoi2::triangle_center(points, delauney, t);
+                let p = vor.triangle_center(t);
                 imageproc::point::Point::new(p.x.round() as i32, p.y.round() as i32)
             }).collect();
             vertices.dedup();
@@ -110,44 +110,43 @@ fn draw_delauney(points: &Vec::<Point>, delauney: &Triangulation, imgx: u32, img
     }
 
     println!("\t\tDrawing Voronoi edges...");
-    for e in 0..delauney.triangles.len() {
-        if e > delauney.halfedges[e] {
-            let p = voronoi2::triangle_center(points, delauney, voronoi2::triangle_of_edge(e));
-            let q = voronoi2::triangle_center(points, delauney, voronoi2::triangle_of_edge(delauney.halfedges[e]));
+    for e in 0..vor.delaunay.triangles.len() {
+        if e > vor.delaunay.halfedges[e] {
+            let p = vor.triangle_center(vor.triangle_of_edge(e));
+            let q = vor.triangle_center(vor.triangle_of_edge(vor.delaunay.halfedges[e]));
 
             img = draw_line_segment(&img, (p.x as f32, p.y as f32), (q.x as f32, q.y as f32), voronoi_corner);
         }
     }
 
     println!("\t\tDrawing Voronoi seeds...");
-    for &Point{x, y} in points.iter() {
-        img = draw_hollow_circle(&img, (x as i32, y as i32), 1, delauney_point);
+    for &Point{x, y} in vor.seeds.iter() {
+        img = draw_hollow_circle(&img, (x as i32, y as i32), 1, delaunay_point);
     }
 
     println!("\t\tDrawing Voronoi corners...");
-    for triangle in 0..delauney.triangles.len() / 3 {
-        let Point{x, y} = voronoi2::triangle_center(points, delauney, triangle);
+    for triangle in 0..vor.delaunay.triangles.len() / 3 {
+        let Point{x, y} = vor.triangle_center(triangle);
 
         img = draw_hollow_circle(&img, (x.round() as i32, y.round() as i32), 1, voronoi_corner);
     }
 
     println!("\t\tSaving...");
-    img.save("map_delauney.png").unwrap();
+    img.save("map_delaunay.png").unwrap();
 }
 
 fn main() {
     let imgx = 400;
     let imgy = 400;
 
-    println!("Generating Delauney triangulation...");
+    println!("Generating delaunay triangulation...");
     let start = Instant::now();
-    let mut rng = Xoshiro256StarStar::seed_from_u64(0);
-    let (points, delauney) = new_delaunay(&mut rng, 256, imgx, imgy);
+    let v = Voronoi::new(0, 256, imgx, imgy);
     let duration = start.elapsed();
     println!("\tDone! ({:.2} seconds)", duration.as_secs_f64());
     let start = Instant::now();
     println!("\tDrawing...");
-    draw_delauney(&points, &delauney, imgx, imgy);
+    draw_delaunay(&v, imgx, imgy);
     let duration = start.elapsed();
     println!("\tDone! ({:.2} seconds)", duration.as_secs_f64());
 
