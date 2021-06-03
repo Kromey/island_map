809342b01a582126e39fc4041ec1bf5aa34e2659
Travis Veazey <travisvz@gmail.com>
2021-06-03 13:54:38 -0800

    Improve/cleanup map erosion code

--- src/map/erosion.rs
+++ src/map/erosion.rs
@@ -10,7 +10,7 @@ use rand_xoshiro::Xoshiro256StarStar;
 
 type Vec2 = na::Vector2<f64>;
 
-/// Time scale for the droplet simulation
+/// Time scale ("Delta Time") for the droplet simulation
 const DT: f64 = 0.8;
 /// Density of a droplet as a function of its volume
 /// Altering the density affects inertia of the droplets
@@ -30,26 +30,22 @@ struct Droplet {
     velocity: Vec2,
     volume: f64,
     sediment: f64,
-    boundary: f64,
 }
 
 impl Droplet {
-    fn new(position: Vec2, boundary: u32) -> Self {
-        let boundary = (boundary - 1) as f64;
-
+    fn new(position: Vec2) -> Self {
         Self {
             position,
             velocity: Vec2::new(0.0, 0.0),
             volume: 1.0,
             sediment: 0.0,
-            boundary,
         }
     }
 
     fn ipos(&self) -> (u32, u32) {
         (
-            self.position[0].clamp(0.0, self.boundary) as u32,
-            self.position[1].clamp(0.0, self.boundary) as u32,
+            self.position[0] as u32,
+            self.position[1] as u32,
         )
     }
 
@@ -59,6 +55,7 @@ impl Droplet {
 }
 
 pub fn erode(elevation: &mut Elevation, rng: &mut Xoshiro256StarStar, cycles: u32) {
+    let bounds = 0.0..(elevation.size() as f64);
     let range = Uniform::new(0, elevation.size());
 
     for _ in 0..cycles {
@@ -71,21 +68,37 @@ pub fn erode(elevation: &mut Elevation, rng: &mut Xoshiro256StarStar, cycles: u3
                 break Vec2::new(x as f64, y as f64);
             }
         };
-        let mut drop = Droplet::new(pos, elevation.size());
+        let mut drop = Droplet::new(pos);
 
         while drop.volume > MIN_VOLUME {
             // Floor the position to find the "cell" the droplet is in
             let ipos = drop.ipos();
-            // Acceleration vector is the x and y components of the normal vector
+
+            // Remove our droplet if it's reached the ocean
+            if elevation[ipos] < SEA_LEVEL {
+                // Deposit all remaining sediment here
+                elevation[ipos] += DT * drop.volume * DEPOSITION_RATE * drop.sediment;
+
+                break;
+            }
+
+            // Get the surface normal to accelerate our droplet
             let normal = elevation.get_normal(ipos.0, ipos.1).xy();
 
-            // Model droplet movement via classical physics
+            // Newtonian Mechanics
             // Accelerate the droplet; F=ma, therefore a=F/m; m=volume*density
-            drop.velocity += DT * normal / (drop.volume * DENSITY);
+            let accel = DT * normal / (drop.volume * DENSITY);
+            drop.velocity += accel;
             // Move the droplet
             drop.position += DT * drop.velocity;
             // Slow the droplet via friction
             drop.velocity *= 1.0 - DT * FRICTION;
+
+            // Kill our droplet if it goes out of bounds
+            if !bounds.contains(&drop.position[0]) || !bounds.contains(&drop.position[1]) {
+                // No need to worry about sediment, it's off the map (and hopefully in the sea)
+                break;
+            }
 
             // Sedimentation
             // Concentration Equilibrium determines how much sediment a drop can hold
@@ -100,17 +113,9 @@ pub fn erode(elevation: &mut Elevation, rng: &mut Xoshiro256StarStar, cycles: u3
             drop.sediment += DT * DEPOSITION_RATE * c_diff;
             elevation[ipos] -= DT * drop.volume * DEPOSITION_RATE * c_diff;
 
-            // Remove our droplet if it reaches the ocean
-            // This also saves having to do bounds checking on position
-            if elevation[ipos] < SEA_LEVEL {
-                // Deposit all remaining sediment here
-                elevation[ipos] += DT * drop.volume * DEPOSITION_RATE * drop.sediment;
-
-                break;
-            }
-
             // Evaporation
             drop.volume *= 1.0 - DT * EVAP_RATE;
+            drop.sediment /= 1.0 - DT * EVAP_RATE; // Conserve sediment mass
         }
     }
 }
